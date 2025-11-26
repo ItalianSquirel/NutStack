4933173c9553874f04ee76a7f6360394415aa9b1
Petie Deveer <50847801+ItalianSquirel@users.noreply.github.com>
2025-11-26 14:01:55 -0900

    Update quick.sh to modify installed applications
    Removed Firefox installation and added several Flatpak applications including GIMP, Spotify, VLC, and others. Also added additional APT installations for tools like ImageMagick and qBittorrent.

--- presets/quick.sh
+++ presets/quick.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 echo "==> Quick installing default tools and apps..."
 
-sudo apt install -y firefox ulauncher htop neofetch
+sudo apt install -y ulauncher htop neofetch
 flatpak install -y flathub org.inkscape.Inkscape
-flatpak install -y flathub com.visualstudio.code
+flatpak install -y flathub org.gimp.GIMP
+flatpak install -y flathub com.bambu.studio
+flatpak install -y flathub com.spotify.Client
+flatpak install -y flathub org.videolan.VLC
+sudo apt install -y imagemagick
+sudo apt install -y rpi-imager
+sudo apt install -y qbittorrent
+sudo apt install -y filezilla
+flatpak install -y flathub com.usebottles.bottles
+flatpak install -y flathub org.blender.Blender
+flatpak install -y flathub org.notepadqq.Notepadqq
+sudo apt install -y gparted
+flatpak run org.localsend.localsend_app
+flatpak run io.github.CyberTimon.RapidRAW
+flatpak run io.github.milkshiift.GoofCord
+flatpak install flathub net.waterfox.waterfox
 
 echo "==> Quick Install Complete!"
